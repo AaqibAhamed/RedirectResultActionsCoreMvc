569fc07a41dcad3b5cf38b1932d11ad6166d1463
Ahamed Aaqib <aaqib@edsmart.com>
2023-12-11 11:19:34 +0530

    Initial Commit

---
+++ RedirectResultActionsCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedirectResultActionsCoreMvc.Models;
+using System.Diagnostics;
+
+namespace RedirectResultActionsCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public RedirectResult MyProfile()
+        {
+            return Redirect("https://www.c-sharpcorner.com/members/farhan-ahmed24");
+        }
+        public RedirectResult Profile()
+        {
+            return RedirectPermanent("https://www.c-sharpcorner.com/members/farhan-ahmed24");
+        }
+
+        public RedirectToActionResult EmployeeList()
+        {
+            return RedirectToAction("Index", "Employees");
+        }
+
+        public RedirectToRouteResult DepartmentList()
+        {
+            return RedirectToRoute(new { action = "Index", controller = "Departments", area = "" });
+        }
+
+        public LocalRedirectResult LocalRedirect()
+        {
+            return LocalRedirect("/Home/Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RedirectResultActionsCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RedirectResultActionsCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
